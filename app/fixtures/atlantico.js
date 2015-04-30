b7816d5ea1b0869b8819527ef4e4c4158fba05d1
Quinn Lee <leekeunhwi@gmail.com>
2015-04-30 11:10:38 -0400

    update atlantico fixture

--- app/fixtures/atlantico.js
+++ app/fixtures/atlantico.js
@@ -1,3 +1,7 @@
+import sparkData from '../fixtures/spark';
+import dotData from '../fixtures/dot';
+import exportData from '../fixtures/exports';
+
 export default {
   name: 'Atlántico',
   id: 'Atlántico',
@@ -5,6 +9,8 @@ export default {
   gdp: '$8.2M',
   population: '2.4M',
   code: '42',
-  randomAttr: { foo: 'bar'}
+  randomAttr: { foo: 'bar'},
+  profileSpark: sparkData.data,
+  profileDot: dotData.data,
+  profileExport: exportData.data
 };
-
