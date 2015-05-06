6566bb94a8c84b9cdbefd78d652e32782b820e7b
gwezerek <gwezerek@gmail.com>
2015-05-06 13:44:07 -0400

    Standardizing spaces and arguments in helpers.

--- app/helpers/format-percent.js
+++ app/helpers/format-percent.js
@@ -1,8 +1,8 @@
 import Ember from 'ember';
 
-export function formatPercent(params/*, hash*/) {
+export function formatPercent(params) {
   var format = d3.format('+.1%');;
-  return format( params );
+  return format(params);
 }
 
 export default Ember.HTMLBars.makeBoundHelper(formatPercent);
