be748a7f39828c24a30c44b3ba935816751def68
Quinn Lee <leekeunhwi@gmail.com>
2015-05-12 12:07:20 -0400

    add spanish stepper story

--- app/locales/es.js
+++ app/locales/es.js
@@ -1,5 +1,26 @@
 export default {
   location : {
     overview: 'Overview in Spanish'
-  }
+  },
+  stepper: {
+    0: `Spanish Spanish Spanish
+      Think of it as a container for all
+      the capabilities needed to extract
+      and transport bananas.`,
+    1: `Spanish Spanish Spanish
+      capabilities needed to extract it can't
+      be used to make many other products. That's
+      why bananas are a low opportunity product.`,
+    2: `Spanish Spanish Spanish
+      capabilities required in their production, given international
+      patterns. %@'s current exports are circled in
+      blue. Follow the links from those exports to see more products
+      that %@ could start exporting`,
+    3: `But %@ doesn't just want more products,
+      Spanish Spanish Spanish
+      that are not common to many countries. This is not the
+      case of bananas, which is produced by almost every country
+      that has banana trees. We shade products green by their
+      complexity—that is, how many countries export them.`
+   }
 };
