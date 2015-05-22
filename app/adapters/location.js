36327ae40be4bdc84e2fdf4124829084c4f9c090
Quinn Lee <leekeunhwi@gmail.com>
2015-05-22 13:57:26 -0400

    refactor location adapter

--- app/adapters/location.js
+++ app/adapters/location.js
@@ -1,19 +1,7 @@
 import ApplicationAdapter from './application';
-import fixture from '../fixtures/atlantico';
-
 
 export default ApplicationAdapter.extend({
   pathForType(type) {
    if (type === 'location') { return 'departments'; }
-  },
-  find(store, type, id, snapshot) {
-    let env = this.container.lookup('application:main').env;
-    return fixture;
-
-    //if(env === 'development'){
-      //return fixture;
-    //}else{
-      //return super.find(store,type,id,snapshot);
-    //}
   }
 });
