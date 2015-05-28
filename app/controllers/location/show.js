bae24019df2707d8e1ba0df2c34c7782a64aab96
Quinn Lee <leekeunhwi@gmail.com>
2015-05-28 15:00:41 -0400

    controller to dictate the lang from model

--- app/controllers/location/show.js
+++ app/controllers/location/show.js
@@ -2,6 +2,15 @@ import Ember from 'ember';
 const {computed} = Ember;
 
 export default Ember.Controller.extend({
+  needs: 'application',
+  isEnglish: Ember.computed.alias("controllers.application.isEnglish"),
+  name: computed('isEnglish',function() {
+    if(this.get('isEnglish')) {
+      return this.model.get('name_en');
+    } else {
+      return this.model.get('name_es');
+    }
+  }),
   activeStep: 0,
   stepStories: computed(function() {
     return [ { index: 0 }, { index: 1 }, { index: 2 }, { index: 3 } ];
