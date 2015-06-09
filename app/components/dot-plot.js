ac4d746e6e9902f7a84aee24f7cdda6a95a81840
Quinn Lee <leekeunhwi@gmail.com>
2015-06-09 09:46:45 -0400

    update dot plot

--- app/components/dot-plot.js
+++ app/components/dot-plot.js
@@ -1,38 +1,42 @@
 import Ember from 'ember';
-const { computed } = Ember;
+const { on,computed } = Ember;
 
 export default Ember.Component.extend({
-  tagName: 'svg',
+  tagName: 'div',
   classNames: ['dotplot'],
   attributeBindings: ['width','height'],
   id: computed('elementId', function() {
     return `#${this.get('elementId')}`;
   }),
-  dotPlot: computed('id','data','w', 'xVar', 'yVar', function() {
+  dotPlot: computed('id','data', function() {
     return vistk.viz()
-      .type('dotplot')
-      .container(this.get('id'))
-      .height(this.get('height'))
-      .width(this.get('width'))
-      .data(this.get('data'))
-      .x_var(this.get('xVar'))
-      .x_scale('linear')
-      .y_var(this.get('yVar'))
-      .id('name')
-      .group('dept_name')
-      .text('dept_name')
-      .connect('name')
-      .focus(0)
-      .time({var_time: 'year', current_time: 2013})
-      .ui(false);
-  }),
+      .params({
+        type: 'dotplot',
+        data: this.get('data'),
+        container: this.get('id'),
+        height: this.get('height'),
+        width: this.get('width'),
+        id: this.get('varId'),
+        var_text: this.get('varText'),
+        var_x: this.get('varX'),
+        x_type: 'linear',
+        x_text: null,
+        x_ticks: 2,
+        x_format: function(d) { return '$' + d3.format(".1s")(d); },
+        mark: { type: 'diamond', width: 10, height: 10, translate: 30 },
+        tickSize: 0,
+        tickPadding: 10
+      });
+ }),
   draw: function() {
-    d3.select(this.get('id'))
-      .call(this.get('dotPlot'));
-  },
-  didInsertElement: function() {
     this.set('width', this.$().parent().width());
     this.set('height', this.$().parent().height());
-    this.draw();
+    d3.select(this.get('id')) .call(this.get('dotPlot'));
+  },
+  didInsertElement: function() {
+    Ember.run.scheduleOnce('afterRender', this , function() {
+      this.draw();
+    });
   }
 });
+
