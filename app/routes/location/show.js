7bfed4fccac28e9a7162da88a904a01a8365b9cd
Quinn Lee <leekeunhwi@gmail.com>
2015-06-10 22:36:12 -0400

    refactor location show route

--- app/routes/location/show.js
+++ app/routes/location/show.js
@@ -5,18 +5,26 @@ export default Ember.Route.extend({
 // `this.store.find` makes an api call for `params.location_id` and returns a promise
 // in the `then` function call, another API call is made to get the topExports data
   model: function(params) {
-    var model = this.store.find('location', params.location_id);
-    var exports = Ember.$.getJSON(`data/products?location=${params.location_id}&year=2012`);
-    var departments = Ember.$.getJSON('data/departments?year=2012');
+    return this.store.find('location', params.location_id);
+  },
+  afterModel: function(model) {
+    if(model.get('level') === 'department') {
+      var exports = Ember.$.getJSON(`data/products?location=${model.id}&year=2012`);
+      var departments = Ember.$.getJSON('data/departments?year=2012');
 
-    return RSVP.allSettled([model, exports, departments]).then(function(array) {
-      let model = array[0].value;
-      let exports = array[1].value;
-      let departments = array[2].value;
+      RSVP.allSettled([exports, departments]).then(function(array) {
+        let exports = array[0].value;
+        let departments = array[1].value;
 
-      if(exports) { model.set('productsData', exports.data);}
-      if(departments) { model.set('departments', departments.data);}
-      return model;
-    });
+        var exportData = exports.data || [];
+        var departmentData = departments.data || [];
+
+        if(exports) { model.set('productsData', exportData);}
+        if(departments) { model.set('departments', departmentData);}
+      });
+    } else {
+       model.set('productsData', []);
+       model.set('departments', []);
+    }
   }
 });
