d07399a0df8afd4467f90288a0dd1f5f6afcce17
Quinn Lee <leekeunhwi@gmail.com>
2015-06-11 15:08:12 -0400

    update brocfile

--- Brocfile.js
+++ Brocfile.js
@@ -21,6 +21,7 @@ var app = new EmberApp({
 app.import('bower_components/d3/d3.min.js');
 app.import('bower_components/lodash/lodash.js');
 app.import('bower_components/vis-toolkit/build/vistk.js');
+app.import('bower_components/d3plus/d3plus.js');
 
 // Use `app.import` to add additional libraries to the generated
 // output files.
