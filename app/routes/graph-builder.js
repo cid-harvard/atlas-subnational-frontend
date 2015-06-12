c038827e1e506d83fde3c6497a2b018eae8d76ca
Quinn Lee <leekeunhwi@gmail.com>
2015-06-12 10:28:50 -0400

    refactor graph builder route

--- app/routes/graph-builder.js
+++ app/routes/graph-builder.js
@@ -1,28 +1,27 @@
 import Ember from 'ember';
 const {RSVP} = Ember;
 
+
 export default Ember.Route.extend({
   queryParams: {
     entity: { refreshModel: true },
-    entity_id: { refreshModel: true }
+    entity_id: { refreshModel: true },
+    data: { refreshModel: true }
   },
-  model: function(transition) {
-    return this.store.find(transition.entity, transition.entity_id);
+  model: function(queryParams, transition) {
+    return this.store.find(queryParams.entity, queryParams.entity_id);
   },
   afterModel: function(model, transition) {
-    var year = Ember.get(transition,'queryParams.year');
-
-    var productsURL = year ? `data/products?location=${model.id}&year=${year}` : `data/products?location=${model.id}`;
-    var departments = Ember.$.getJSON(`data/departments?year=${year}`);
-    var products = Ember.$.getJSON(productsURL);
-
-    return RSVP.allSettled([products, departments]).then(function(array) {
-      var productsData = Ember.get(array[0], 'value.data') || [];
-      var departmentData = Ember.get(array[1], 'value.data') || [];
-      model.set('productsData', productsData);
-      model.set('departments', departmentData);
-      return model;
-    });
+    var data = Ember.getWithDefault(transition, 'queryParams.data', 'product');
+    if(data  === 'product'){
+      this.productsData(model);
+    }
+  },
+  productsData: function(model) {
+    Ember.$.getJSON(`data/products?location=${model.id}`)
+      .then(function(data) {
+        model.set('productsData', Ember.getWithDefault(data, 'data', []));
+      });
   }
 });
 
