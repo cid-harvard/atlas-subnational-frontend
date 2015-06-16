e83d02e6f8eadfa9f87dc507b12a1b6b5ceb9b5f
Quinn Lee <leekeunhwi@gmail.com>
2015-06-16 15:11:39 -0400

    create d3-plus-scatter

---
+++ app/components/d3-plus-scatter.js
@@ -0,0 +1,35 @@
+import Ember from 'ember';
+const {computed, observer} = Ember;
+
+export default Ember.Component.extend({
+  tagName: 'div',
+  id: computed('elementId', function() {
+    return `#${this.get('elementId')}`;
+  }),
+  scatter: computed('data', 'id', function() {
+    var maxYear = d3.max(this.get('data'), function(d) {return d.year;} );
+    return d3plus.viz()
+    .container(this.get('id'))
+    .data({value: this.get('data')})
+    .type("scatter")
+    .color('grey')
+    .id(this.get('varId'))
+    .x(this.get('varX'))
+    .y(this.get('varY'))
+    .size(this.get('varSize'))
+    .time({'value': 'year', 'solo': maxYear })
+    .timeline(false)
+    .height(this.get('height'))
+    .width(this.get('width'))
+  }),
+  didInsertElement: function() {
+    Ember.run.scheduleOnce('afterRender', this , function() {
+      this.set('width', this.$().parent().width());
+      this.set('height', this.$().parent().height());
+      this.get('scatter').draw();
+    });
+  },
+  didDataChange: observer('data', function() {
+    this.rerender();
+  })
+});
