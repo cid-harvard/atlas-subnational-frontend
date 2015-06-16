a446c690bc3be02e501e8908a30fc197c7f03fb0
Quinn Lee <leekeunhwi@gmail.com>
2015-06-16 10:46:44 -0400

    add placeholder url to src

--- config/environment.js
+++ config/environment.js
@@ -26,6 +26,7 @@ module.exports = function(environment) {
     ENV.contentSecurityPolicy = {
       'connect-src': "'self' http://54.6.95.239 *",
       'style-src': "'self' 'unsafe-inline' *",
+      'img-src': "'self' http://placehold.it/40x40 *"
     }
   }
 
@@ -45,7 +46,8 @@ module.exports = function(environment) {
     ENV.baseURL = '/atlas-colombia',
     ENV.locationType = 'hash',
     ENV.contentSecurityPolicy = {
-      'connect-src': "'self' http://54.6.95.239 *"
+      'connect-src': "'self' http://54.6.95.239 *",
+      'img-src': "'self' http://placehold.it/40x40 *"
     }
   }
 
