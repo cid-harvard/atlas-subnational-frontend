0333f91142bd4bdc52afef5b39b288db1d88b0b1
romsson <romain.vuillemot@gmail.com>
2015-06-22 16:31:30 -0400

    Updated parameters and style

--- app/components/dot-plot.js
+++ app/components/dot-plot.js
@@ -16,19 +16,20 @@ export default Ember.Component.extend({
         container: this.get('id'),
         height: this.get('height'),
         width: this.get('width'),
-        margin: {top: 0, right: 10, bottom: 0, left: 10},
+        margin: {top: 0, right: 20, bottom: 20, left: 20},
         var_id: this.get('varId'),
-        var_text: this.get('varText'),
         var_x: this.get('varX'),
-        x_type: 'linear',
+        var_y: function() { return this.height/2; },
         x_text: null,
         x_ticks: 2,
-        x_format: function(d) { return '$' + d3.format(".1s")(d); },
-        mark: { type: 'diamond', width: 10, height: 10, translate: 30 },
-        tickSize: 0,
-        tickPadding: 10,
-        selection: ["Antioquia"],
-        highlight: ["Antioquia"],
+        x_format: function(d) { return '$' + d3.format(".2s")(d); },
+        items: [{
+          marks: [{
+            type: "diamond"
+          }]
+        }],
+        selection: [this.get('currentLocation')],
+        highlight: [this.get('currentLocation')],
       });
  }),
   draw: function() {
