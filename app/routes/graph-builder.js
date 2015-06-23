c5d23a5c5d784143d1816f043ca534e9efaa33c1
Quinn Lee <leekeunhwi@gmail.com>
2015-06-23 15:22:48 -0400

    COL-253 get products complexity data

--- app/routes/graph-builder.js
+++ app/routes/graph-builder.js
@@ -22,19 +22,24 @@ export default Ember.Route.extend({
     }
   },
   setProducts: function(model){
-    return Ember.$.getJSON(`${apiURL}data/products?location=${model.id}`)
-      .then((json) => {
-        var productsMetadata = this.modelFor('application').products;
-        var data = Ember.getWithDefault(json, 'data', []);
-        _.each(data, function(d) {
-          let product = _.find(productsMetadata, { id: d.product_id});
-          d.name = product.name_en;
-          _.extend(d, product);
-          d.group = product.group;
-          d.group_name_en = product.group_name_en;
-        });
-        model.set('productsData', data);
-      })
+    var products = Ember.$.getJSON(`${apiURL}data/products?location=${model.id}`);
+    var productsData = Ember.$.getJSON(`${apiURL}data/products/scatterplot?location=${model.id}&year=2012`);
+    return Ember.RSVP.allSettled([products, productsData])
+      .then((array) => {
+        let products = Ember.getWithDefault(array[0], 'value.data', []);
+        let productsData = Ember.getWithDefault(array[1], 'value.data', []);
+        let productsMetadata = this.modelFor('application').products;
+
+          _.each(products, function(d) {
+            let product = _.find(productsMetadata, { id: d.product_id });
+            let productData = _.find(productsData, { product_id: d.product_id });
+            d.name = product.name_en;
+            _.extend(d, product);
+            _.extend(d, productData);
+          });
+
+          model.set('productsData', products);
+        })
       .then(function() { window.scrollTo(0,0);});
   },
   setIndustries: function(model) {
@@ -43,20 +48,21 @@ export default Ember.Route.extend({
     var industriesData = Ember.$.getJSON(`${apiURL}data/industries/scatterplot?location=${model.id}&year=2012`);
     return Ember.RSVP.allSettled([industries, industriesData])
       .then((array) => {
-      let industries = Ember.getWithDefault(array[0], 'value.data', []);
-      let industriesData = Ember.getWithDefault(array[1], 'value.data', []);
-      let industriesMetadata = this.modelFor('application').industries;
+        let industries = Ember.getWithDefault(array[0], 'value.data', []);
+        let industriesData = Ember.getWithDefault(array[1], 'value.data', []);
+        let industriesMetadata = this.modelFor('application').industries;
 
-      _.each(industries, function(d) {
-        let industry = _.find(industriesMetadata, { id: d.industry_id });
-        let industryData = _.find(industriesData, { industry_id: d.industry_id });
-        d.name = industry.name_en;
-        _.extend(d, industry);
-        _.extend(d, industryData);
-      });
+        _.each(industries, function(d) {
+          let industry = _.find(industriesMetadata, { id: d.industry_id });
+          let industryData = _.find(industriesData, { industry_id: d.industry_id });
+          d.name = industry.name_en;
+          _.extend(d, industry);
+          _.extend(d, industryData);
+        });
 
-      model.set('industriesData', industries);
-    }).then(function() { window.scrollTo(0,0);});
+        model.set('industriesData', industries);
+      })
+      .then(function() { window.scrollTo(0,0);});
    }
 });
 
