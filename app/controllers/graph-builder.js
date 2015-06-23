76b05c81af10ed99ad4fb84d8568d93ed8d41860
Quinn Lee <leekeunhwi@gmail.com>
2015-06-23 10:44:49 -0400

    COL-253 Decorate object for graphbuilder

--- app/controllers/graph-builder.js
+++ app/controllers/graph-builder.js
@@ -16,6 +16,32 @@ export default Ember.Controller.extend({
   departmentLocations: computed('locationsMetadata', function(){
     return _.filter(this.get('locationsMetadata'), 'level', 'department');
   }),
+  data: computed('source','vis', function() {
+    let source = this.get('source')
+    if(source  === 'products') {
+      return this.get('model.productsData');
+    } else if(source === 'industries') {
+      return  this.get('vis') === 'scatter' ? this.get('scatterPlot') : this.get('model.industriesData');
+    }
+  }),
+  visualizationComponent: computed('vis', function(){
+    let visualization = this.get('vis');
+    if ( visualization === 'treemap') {
+      return 'd3plus-tree-map';
+    } else if (visualization === 'multiples') {
+      return 'multiples-graph';
+    } else if (visualization === 'scatter') {
+      return 'd3plus-scatter';
+    }
+  }),
+  actions: {
+    toTreemap: function() {
+      this.set('vis', 'treemap');
+    },
+    toMultiples: function() {
+      this.set('vis', 'multiples');
+    }
+  }
 });
 
 
