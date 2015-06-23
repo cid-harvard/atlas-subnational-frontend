fa0ae090931244e0f9a1060eb6786a292c141fd1
Quinn Lee <leekeunhwi@gmail.com>
2015-06-23 10:44:23 -0400

    COL-253 refactor all data munging to route
    COL-264 set rca <= 1

--- app/controllers/graph-builder.js
+++ app/controllers/graph-builder.js
@@ -9,74 +9,13 @@ export default Ember.Controller.extend({
   variable: 'export_value',
 
   isEnglish: computed.alias('controllers.application.isEnglish'),
-  productsMetadata: computed.alias('controllers.application.productsMetadata'),
-  locationsMetadata: computed.alias('controllers.application.locationsMetadata'),
-  industriesMetadata: computed.alias('controllers.application.industriesMetadata'),
-  totalWages: computed('industriesData', function() {
-    let data = _.filter(this.get('industriesData'), {year: 2012});
-    return _.sum(data, 'wages').toString().replace(/(\d)(?=(\d{3})+(?!\d))/g, "$1,");
-  }),
-  totalExports: computed('productsData', function() {
-    let data = _.filter(this.get('productsData'), {year: 2012});
-    return  _.sum(data, 'export_value').toString().replace(/(\d)(?=(\d{3})+(?!\d))/g, "$1,");
-  }),
-  isProducts: computed('data_source', function() {
-    return this.get('data_source') === 'products';
-  }),
-  isIndustries: computed('data_source', function() {
-    return this.get('data_source') === 'industries';
-  }),
-  filterRca: function( data ) {
-      // TODO: Eventually move this into builder tools for scatter and similarity map
-      return _.filter( data, function(d) { return d.rca >= 1; });
-  },
-  productsData: computed('model.productsData', function() {
-    let products = this.get('model.productsData');
-    let productsMetadata = this.get('productsMetadata');
-    _.each(products, function(d) {
-      let product = _.find(productsMetadata, { id: d.product_id});
-      let parentProduct =  _.find(productsMetadata, { id: product.parent_id });
-      d.name = product.name_en;
-      d.level = product.level;
-      d.code = product.code;
-      d.parent_id = parentProduct.id;
-      d.parent_name = parentProduct.name_en;
-      d.parent_code = parentProduct.code;
-    });
-    return products;
-  }),
-  industriesData: computed('model.industriesData', function() {
-    let industries = this.get('model.industriesData');
-    let industriesMetadata = this.get('industriesMetadata');
-    _.each(industries, function(d) {
-      let industry = _.find(industriesMetadata, { id: d.industry_id });
-      let parentIndustry =  _.find(industriesMetadata, { id: industry.parent_id});
-      d.name = industry.name_en;
-      d.code = industry.code;
-      d.level = industry.level;
-      d.parent_id = parentIndustry.id;
-      d.parent_name = parentIndustry.name_en;
-      d.parent_code = parentIndustry.code;
-    });
-    return industries;
-  }),
-  scatterPlot: computed('model.scatterPlot', function() {
-    let industries = _.filter(this.get('industriesData'), {year: 2012});
-    let scatterPlot = this.get('model.scatterPlot');
-    _.each(scatterPlot, function(d) {
-      let industry = _.find(industries, { industry_id: d.industry_id });
-      d.name = industry.name;
-      d.rca = industry.rca;
-    });
-    scatterPlot = this.filterRca(scatterPlot);
-    return scatterPlot;
+
+  scatterPlot: computed('model.industriesData', function() {
+    return _.filter(this.get('model.industriesData'), function(d) { return d.rca <= 1; });
   }),
   departmentLocations: computed('locationsMetadata', function(){
     return _.filter(this.get('locationsMetadata'), 'level', 'department');
   }),
-  scrollTop: observer('data', function() {
-    window.scrollTo(0,0);
-  })
 });
 
 
--- app/routes/graph-builder.js
+++ app/routes/graph-builder.js
@@ -1,5 +1,6 @@
 import Ember from 'ember';
 import ENV from '../config/environment';
+const {computed} = Ember;
 const {apiURL} = ENV;
 
 export default Ember.Route.extend({
@@ -14,21 +15,49 @@ export default Ember.Route.extend({
     return this.store.find(queryParams.entity, queryParams.entity_id);
   },
   afterModel: function(model, transition) {
-    var data = Ember.getWithDefault(transition, 'queryParams.data_source', 'products');
+    var data = Ember.getWithDefault(transition, 'queryParams.source', 'products');
     if(data  === 'products'){
-      return Ember.$.getJSON(`${apiURL}data/products?location=${model.id}`)
-        .then(function(data) { model.set('productsData', Ember.getWithDefault(data, 'data', [])); })
-        .then(function() { window.scrollTo(0,0);});
-    } else if (data === 'industries') {
-      var industryData = Ember.$.getJSON(`${apiURL}data/industries?location=${model.id}`);
-      var scatterPlot = Ember.$.getJSON(`${apiURL}data/industries/scatterplot?location=${model.id}&year=2012`);
-      return Ember.RSVP.allSettled([industryData, scatterPlot]).then(function(array) {
-        var data = array[0];
-        var scatter = array[1];
-        model.set('industriesData', Ember.getWithDefault(data, 'value.data', []));
-        model.set('scatterPlot', Ember.getWithDefault(scatter, 'value.data', []));
-      }).then(function() { window.scrollTo(0,0);});
+      return this.setProducts(model);
+    } if (data === 'industries') {
+      return this.setIndustries(model);
     }
-  }
+  },
+  setProducts: function(model){
+    return Ember.$.getJSON(`${apiURL}data/products?location=${model.id}`)
+      .then((json) => {
+        var productsMetadata = this.modelFor('application').products;
+        var data = Ember.getWithDefault(json, 'data', []);
+        _.each(data, function(d) {
+          let product = _.find(productsMetadata, { id: d.product_id});
+          d.name = product.name_en;
+          _.extend(d, product);
+          d.group = product.group;
+          d.group_name_en = product.group_name_en
+        });
+        model.set('productsData', data);
+      })
+      .then(function() { window.scrollTo(0,0);});
+  },
+  setIndustries: function(model) {
+    var industries = Ember.$.getJSON(`${apiURL}data/industries?location=${model.id}`);
+    // industriesData has the RCA and Complexity
+    var industriesData = Ember.$.getJSON(`${apiURL}data/industries/scatterplot?location=${model.id}&year=2012`);
+    return Ember.RSVP.allSettled([industries, industriesData])
+      .then((array) => {
+      let industries = Ember.getWithDefault(array[0], 'value.data', [])
+      let industriesData = Ember.getWithDefault(array[1], 'value.data', [])
+      let industriesMetadata = this.modelFor('application').industries;
+
+      _.each(industries, function(d) {
+        let industry = _.find(industriesMetadata, { id: d.industry_id });
+        let industryData = _.find(industriesData, { industry_id: d.industry_id });
+        d.name = industry.name_en;
+        _.extend(d, industry);
+        _.extend(d, industryData);
+      });
+
+      model.set('industriesData', industries);
+    }).then(function() { window.scrollTo(0,0);});
+   }
 });
 
