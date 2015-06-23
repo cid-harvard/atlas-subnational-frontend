8961cd7eb4c541363a21410d5d36a74a390bc4eb
Quinn Lee <leekeunhwi@gmail.com>
2015-06-23 10:45:28 -0400

    COL-271 Get 1 digit values for scatter plot

--- app/routes/application.js
+++ app/routes/application.js
@@ -9,21 +9,46 @@ export default Ember.Route.extend({
     var productsMetadata = Ember.$.getJSON(apiURL+'metadata/products/');
     var locationsMetadata = Ember.$.getJSON(apiURL+'metadata/locations/');
     var productsHierarchy = Ember.$.getJSON(apiURL+'metadata/products/hierarchy?from_level=4digit&to_level=section');
-    var industiesMetadata = Ember.$.getJSON(apiURL+'metadata/industries');
-
-    var promises = [productsMetadata, locationsMetadata, productsHierarchy, industiesMetadata];
+    var industriesMetadata = Ember.$.getJSON(apiURL+'metadata/industries');
+    var industriesHierarchy = Ember.$.getJSON(apiURL+'metadata/industries/hierarchy?from_level=4digit&to_level=section');
+    var promises = [productsMetadata, locationsMetadata, productsHierarchy, industriesMetadata, industriesHierarchy];
 
     return RSVP.allSettled(promises).then(function(array) {
       let productsMetadata = array[0].value.data;
       let locationsMetadata = array[1].value.data;
       let productMap = array[2].value.data;
-      let industiesMetadata = array[3].value.data;
+      let industriesMetadata = array[3].value.data;
+      let industriesMap = array[4].value.data;
+
+      // Finds the entity with the `1st digit` that matches
+      // sets `group` to the `1st digit code`
+      // `group_name` to the name of the entity
 
       _.forEach(productsMetadata, function(d) {
-        d.group = productMap[d.product_id];
+        let productId = productMap[d.id];
+        if(productId){
+          let product = _.find(productsMetadata, {id:  productId});
+          d.group = productId;
+          d.group_name_en = product.name_en;
+        } else {
+          d.group = d.id;
+          d.group_name_en = d.name_en;
+        }
       });
 
-      return { products: productsMetadata, locations: locationsMetadata, industries: industiesMetadata };
+      _.forEach(industriesMetadata, function(d) {
+        let industryId = industriesMap[d.id];
+        if(industryId){
+          let industry = _.find(industriesMetadata, { id: industryId });
+          d.group = industryId;
+          d.group_name_en = industry.name_en;
+        } else {
+          d.group = d.id;
+          d.group_name_en = d.name_en;
+        }
+      });
+
+      return { products: productsMetadata, locations: locationsMetadata, industries: industriesMetadata };
     });
   },
   actions: {
