cdb72aac6fe085b1ea5164c3273d1b4271ba4c94
romsson <romain.vuillemot@gmail.com>
2015-06-23 12:15:39 -0400

    Updates parameters for treemap
    Note: nesting remains to be added

--- app/components/tree-map.js
+++ app/components/tree-map.js
@@ -11,23 +11,21 @@ export default Ember.Component.extend({
     return this.get('data');
   }),
   treemap: computed('id','data',function() {
-     return  vistk.viz()
-       .container(this.get('id'))
-       .params({
-         type: "treemap",
-         container: this.get('id'),
-         height: this.get('height'),
-         width: this.get('width'),
-         margin: {top: 10, right: 10, bottom: 10, left: 10},
-         data: this.get('filteredData'),
-         var_id: this.get('varId'),
-         var_group: this.get('varGroup'),
-         color: function() { return "#bbb"; },
-         var_size: this.get('varSize'),
-         var_text: this.get('varText'),
-         mark: { type: 'rect', width: 10, height: 10 },
-         focus: 1121
-       });
+    return  vistk.viz()
+      .container(this.get('id'))
+      .params({
+        type: "treemap",
+        container: this.get('id'),
+        height: this.get('height'),
+        width: this.get('width'),
+        margin: {top: 0, right: 0, bottom: 0, left: 0},
+        data: this.get('filteredData'),
+        var_id: this.get('varId'),
+        var_group: this.get('varGroup'),
+        var_size: this.get('varSize'),
+        var_text: this.get('varText'),
+        color: function() { return "#EAE6E3"; }
+      });
   }),
   draw: function() {
     this.set('width', this.$().parent().width());
