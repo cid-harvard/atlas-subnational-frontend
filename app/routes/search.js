a4239774434dff9dcaf2fcaa4a1bf3869463cecb
Quinn Lee <leekeunhwi@gmail.com>
2015-06-26 17:28:49 -0400

    COL-287 get muni and implement search in route

--- app/routes/search.js
+++ app/routes/search.js
@@ -1,4 +1,7 @@
 import Ember from 'ember';
+import ENV from '../config/environment';
+const {get: get} = Ember;
+const {apiURL} = ENV;
 
 export default Ember.Route.extend({
   queryParams: {
@@ -15,9 +18,21 @@ export default Ember.Route.extend({
     }
   },
   model: function(transition) {
-    var url = 'http://52.6.95.239/api/';
+    var departments = Ember.$.getJSON(`${apiURL}metadata/locations/?level=department`);
+    var municipalities = Ember.$.getJSON(`${apiURL}metadata/locations/?level=municipality`);
+    let search = _.deburr(transition.query);
+    var regexp = new RegExp(search.replace(/(\S+)/g, function(s) { return "\\b(" + s + ")(.*)"; })
+      .replace(/\s+/g, ''), "gi");
+
     if(transition.query) {
-      return Ember.$.getJSON(url+'metadata/locations/?level=department')
-        .then(function(model) { return model.data; });
+      return Ember.RSVP.allSettled([departments, municipalities])
+        .then(function(array) {
+          return _.chain(array)
+            .map(function(d){return d.value.data})
+            .flatten()
+            .filter(function(d){ return _.deburr(get(d,'name_en')).match(regexp) || get(d, 'code').match(regexp)})
+            .value()
+        });
     }
-  }});
+  }
+});
