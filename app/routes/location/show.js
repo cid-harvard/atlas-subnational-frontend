da074bea573f48a930c32508ec034bf91a7c7f5b
Quinn Lee <leekeunhwi@gmail.com>
2015-06-30 10:51:28 -0400

    fix error on join, id =/= department_id

--- app/routes/location/show.js
+++ app/routes/location/show.js
@@ -36,7 +36,7 @@ export default Ember.Route.extend({
 
       //all department data for 2012
       _.each(departmentsData, function(d) {
-        let department = _.find(departmentsDataAll, {id: d.department_id, year: 2012});
+        let department = _.find(departmentsDataAll, {department_id: d.department_id, year: 2012});
         d.name = _.find(locationsMetadata, {id: d.department_id}).name_en;
         _.extend(d, department);
       });
