87d68d55f33082b9d1ad7d39667d8bda6ab9ad2d
Quinn Lee <leekeunhwi@gmail.com>
2015-07-01 10:06:25 -0400

    refactor, no need to have entity independent adapters

--- app/adapters/location.js
+++
@@ -1,7 +0,0 @@
-import ApplicationAdapter from './application';
-
-export default ApplicationAdapter.extend({
-  pathForType(type) {
-   if (type === 'location') { return 'metadata/locations'; }
-  }
-});
