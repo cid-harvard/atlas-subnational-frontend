98d158b54086f8886e758fafd4850d020f2b2897
Quinn Lee <leekeunhwi@gmail.com>
2015-07-07 11:43:35 -0400

    COL-289 refactor input slider to be more declarative

--- app/components/input-slider.js
+++ app/components/input-slider.js
@@ -1,9 +1,15 @@
 import Ember from 'ember';
+const {computed} = Ember;
 
 export default Ember.Component.extend({
   classNames: ['settings__input'],
-  slider: {},
-  sliderOptions: Ember.computed('type', function(){
+  minDate: computed('dateRange', function() {
+    return this.get('dateRange')[0];
+  }),
+  maxDate: computed('dateRange', function() {
+    return this.get('dateRange')[1] + 1;
+  }),
+  sliderOptions: computed('type','dateRange', function() {
     return {
       start: [this.get('startDate'), this.get('endDate')],
       step: 1,
@@ -11,94 +17,37 @@ export default Ember.Component.extend({
       connect: true,
       behaviour: 'tap-drag',
       range: {
-        'min': 2006,
-        'max': 2014
+        'min': this.get('minDate'),
+        'max': this.get('maxDate')
       },
       pips: {
         mode: 'count',
-        values: 9
+        values: 1 + (this.get('maxDate') - this.get('minDate'))
       },
       format: {
-        to: function (value) {
-          return value;
-        },
-        from: function (value) {
-          return value.replace(',-', '');
-        }
+        to: function(value) { return value; },
+        from: function(value) {  return value; }
       }
     };
   }),
-  initSlider: function() {
-    // this.setSliderConfig();
-    this.createSlider();
-    this.bindListeners();
-  },
-  setSliderConfig: function() {
-    // let type = this.get('type');
-
-    // if(type === 'time') {
-    //   this.set('sliderOptions', {
-    //     start: [this.get('startDate'), this.get('endDate')],
-    //     step: 1,
-    //     margin: 1,
-    //     connect: true,
-    //     behaviour: 'tap-drag',
-    //     range: {
-    //       'min': 2006,
-    //       'max': 2014
-    //     },
-    //     pips: {
-    //       mode: 'count',
-    //       values: 9
-    //     }
-    //   });
-    // } else if(type === 'rca' || type === 'similarity') {
-    //   this.set('sliderOptions', {
-    //     start: [1],
-    //     step: 1,
-    //     behaviour: 'tap',
-    //     range: {
-    //       'min': 0,
-    //       'max': 10
-    //     },
-    //     pips: {
-    //       mode: 'count',
-    //       values: 11
-    //     }
-    //   });
-    // }
-  },
-  createSlider: function() {
-    this.slider = document.getElementById(this.elementId);
-    let opt = this.get('sliderOptions');
-
-    noUiSlider.create(this.slider, {
-      start: opt.start, // Handle start position
-      step: opt.step, // Slider moves in increments of '10'
-      margin: opt.margin, // Handles must be more than one step apart
-      connect: opt.connect, // Display a colored bar between the handles
-      behaviour: opt.behaviour, // Move handle on tap, bar is draggable
-      range: opt.range, // Slider can select '0' to '100',
-      pips: opt.pips, // Show a scale with the slider
-      format: opt.format // Convert vals from floats to ints
-    });
-  },
-  setSliderRange: function() {
-    // Get array with range of all years
-  },
-  bindListeners: function() {
-    this.slider.noUiSlider.on('update', (values)=> {
-      if(this.get('type') === 'time') {
-        this.set('startDate', values[0]);
-        this.set('endDate', values[1]);
-      } else if(this.get('type') === 'rca') {
-        this.set('rca', values[0]);
-      } else if(this.get('type') === 'similarity') {
-        this.set('rca', values[0]);
-      }
-    });
-  },
+  sliderSetterFunction: computed('type', function() {
+    let type = this.get('type');
+    if(type === 'time') {
+      return (values) => {
+        this.set('startDate', parseInt(values[0]));
+        this.set('endDate', parseInt(values[1]));
+      };
+    } else  { //rca, similarity
+      return (values) => {
+        this.set('rca', parseInt(values[0]));
+      };
+    }
+  }),
   didInsertElement: function() {
-    this.initSlider();
+    Ember.run.scheduleOnce('afterRender', this , function() {
+      noUiSlider.create(this.element, this.get('sliderOptions'))
+      this.element.noUiSlider
+        .on('set', this.get('sliderSetterFunction'));
+    });
   }
 });
