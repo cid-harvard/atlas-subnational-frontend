68749a1a773cb2daec05e2d23b2a3e0f6cabd4b9
Quinn Lee <leekeunhwi@gmail.com>
2015-07-16 19:19:26 -0400

    refactor metadata calls

--- app/routes/application.js
+++ app/routes/application.js
@@ -6,11 +6,11 @@ const {apiURL} = ENV;
 export default Ember.Route.extend({
   model: function() {
 
-    var productsMetadata = Ember.$.getJSON(apiURL+'metadata/products/');
-    var locationsMetadata = Ember.$.getJSON(apiURL+'metadata/locations/');
-    var productsHierarchy = Ember.$.getJSON(apiURL+'metadata/products/hierarchy?from_level=4digit&to_level=section');
-    var industriesMetadata = Ember.$.getJSON(apiURL+'metadata/industries');
-    var industriesHierarchy = Ember.$.getJSON(apiURL+'metadata/industries/hierarchy?from_level=4digit&to_level=section');
+    var productsMetadata = Ember.$.getJSON(apiURL+'/metadata/products/');
+    var locationsMetadata = Ember.$.getJSON(apiURL+'/metadata/locations/');
+    var productsHierarchy = Ember.$.getJSON(apiURL+'/metadata/products/hierarchy?from_level=4digit&to_level=section');
+    var industriesMetadata = Ember.$.getJSON(apiURL+'/metadata/industries');
+    var industriesHierarchy = Ember.$.getJSON(apiURL+'/metadata/industries/hierarchy?from_level=4digit&to_level=section');
     var promises = [productsMetadata, locationsMetadata, productsHierarchy, industriesMetadata, industriesHierarchy];
 
     return RSVP.allSettled(promises).then(function(array) {
