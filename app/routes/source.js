487754d3f740a0338d1c0530d91f7a1a8c93c60c
Quinn Lee <leekeunhwi@gmail.com>
2015-07-20 10:49:48 -0400

    fix jshint error

--- app/routes/source.js
+++ app/routes/source.js
@@ -4,7 +4,7 @@ export default Ember.Route.extend({
   model(params) {
     let entity = this.modelFor('graph-builder');
     let source_type = params.source_type;
-    return entity.get(`graphbuilder${source_type.capitalize()}`)
+    return entity.get(`graphbuilder${source_type.capitalize()}`);
   },
   afterModel(model) {
     if(Ember.get(model, 'data')) {
