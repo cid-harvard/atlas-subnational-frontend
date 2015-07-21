9d7a2cf7e9e39a3006750166a9289075f08d3cbc
gwezerek <gwezerek@gmail.com>
2015-07-21 12:22:19 -0400

    COL-336: Created routes for About subpages.

---
+++ tests/unit/routes/about/glossary-test.js
@@ -0,0 +1,11 @@
+import { moduleFor, test } from 'ember-qunit';
+
+moduleFor('route:about/glossary', 'Unit | Route | about/glossary', {
+  // Specify the other units that are required for this test.
+  // needs: ['controller:foo']
+});
+
+test('it exists', function(assert) {
+  var route = this.subject();
+  assert.ok(route);
+});
