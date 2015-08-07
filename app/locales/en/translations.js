8a2795d804a2d5591d2281bc3da225aab994aa43
Quinn Lee <leekeunhwi@gmail.com>
2015-08-07 13:57:53 -0400

    COL-460 update translation en file

--- app/locales/en/translations.js
+++ app/locales/en/translations.js
@@ -27,12 +27,17 @@ export default {
     'download': 'Download this data'
   },
   'search': {
+    'header': 'Search',
+    'intro': "First, search for the location, product, industry or occupation that you're interested in.",
+    'placeholder': "Type here to search",
     'results_products': 'Results: Products',
     'results_locations': 'Results: Locations',
     'results_industries': 'Results: Industries',
     'didnt_find': "Didn't find what you were looking for? Let us know: TKTK@prosperia.com"
   },
   'graph_builder': {
+    'read_more': 'Read the profile and complexity analysis for this location',
+    'view_more': 'View More',
     'table': {
       'name': 'Name',
       'export_value': 'Export',
@@ -59,41 +64,18 @@ export default {
     'change_graph': {
       'label': 'Change Graph',
     },
-    'page_title': {
-      'product': {
-        'locations.export_value': 'What departments export {{name}}?',
-      },
-      'location': {
-        'products.export_value': 'What products does the {{level}} of {{name}} export?',
-        'products.scatter': 'What products have the best combination of complexity and opportunity for the {{level}} of {{name}}?',
-        'products.export_value_to': 'What products does {{name}} export to {{place}}?',
-        'products.import_value': 'What products does the {{level}} of {{name}} import?',
-        'products.import_value_from': 'What products does the {{level}} of {{name}} import from {{place}}?',
-        'products.similarity': 'What does the product map look like for the {{level}} of {{name}}?',
-        'industries.employment': 'What industries in the {{level}} of {{name}} employ the most people?',
-        'industries.scatter': 'What industries have the best combination of complexity and opportunity for the {{level}} of {{name}}?',
-        'industries.wages': 'What industries offer the highest wages for {{name}}?',
-        'industries.wages_highest_per_worker': 'What industries in the {{level}} of {{name}} have the highest wages per worker?',
-        'industries.similarity': 'What does the industry map look like for the {{level}} of {{name}}?',
-        'locations.export': 'What countries does the {{level}} of {{name}} export to?',
-        'locations.export_to': 'What countries does the {{level}} of {{name}} export {{item}} to?',
-        'locations.import_from': 'What countries does the {{level}} of {{name}} import from?',
-        'locations.import_product_from': 'What countries does the {{level}} of {{name}} import {{item}} from?',
-        'locations.export_subregions': 'What subregions contribute to the exports from the {{level}} of {{name}}?',
-        'locations.export_subregions_products': 'What subregions contribute to the exports of {{item}} from the {{level}} of {{name}}?',
-        'locations.export_subregions_locations':'What subregions contribute to the exports from the {{level}} of {{name}} to {{place}}?',
-        'locations.export_subregions_products_locations':' What subregions contribute to the exports of {{item}} from the {{level}} of {{name}} to {{place}}?',
-        'locations.import_subregions': 'What subregions contribute to the imports for the {{level}} of {{name}}?',
-        'locations.import_subregions_products': 'What subregions contribute to the imports of {{item}} for the {{level}} of {{name}}?',
-        'locations.import_subregions_locations': 'What subregions contribute to the imports for the {{level}} of {{name}} from {{place}}?',
-        'locations.import_subregions_products_locations': 'What subregions contribute to the imports of {{item}} for the {{level}} of {{name}} from {{place}}?'
-      }
+    'multiples': {
+      'show_all': 'Show All',
     },
-    'builder_nav': {
-      'header': 'More graphs for this {{entity_type}}',
-      'intro': "Select a question to see the corresponding graph. If the question has missing parameters ({{icon}}) , you'll fill those in when you click.",
+    'page_title': {
+      'industry': {
+        'departments.employment': 'What departments does {{name}} employ the most people',
+        'departments.wages': 'What departments does {{name}} pay the highest total wages',
+        'departments.wage_avg': 'What departments does {{name}} pay the highest average wages',
+      },
       'product': {
         'locations.export_value': 'What departments the export {{name}}?',
+        'locations.import_value': 'What departments the export {{name}}?',
       },
       'location': {
         'products.export_value': 'What products does {{name}} export?',
@@ -121,7 +103,16 @@ export default {
         'locations.import_subregions_products_locations': 'What subregions contribute to the imports of {{item}} for {{name}} from {{place}}?'
       }
     },
+    'builder_nav': {
+      'header': 'More graphs for this {{entity_type}}',
+      'intro': "Select a question to see the corresponding graph. If the question has missing parameters ({{icon}}) , you'll fill those in when you click.",
+    },
     'builder_mod_header': {
+      'industry': {
+        'departments.employment': 'Total Employment',
+        'departments.wages': 'Total Wages (COP)',
+        'departments.wage_avg': 'Average Wages (COP)',
+      },
       'product': {
         'locations.export_value': 'Departments of Colombia',
       },
@@ -139,6 +130,23 @@ export default {
   },
   'location.show': {
     'overview': 'Overview',
+    'bullet.gdp_grow_rate': 'GDP grew {{gdpGrowth}} between {{yearRange}}',
+    'bullet.gdp_pc': '{{name}} has a GDP per capita at {{lastGdpPerCapita}}',
+    'bullet.last_pop': 'The population is {{lastPop}}',
+    'all_departments': 'All 32 departments',
+    'value': 'Value',
+    'growth_annual': 'Growth, annual ({{yearRange}})',
+    'gdp_pc': 'GDP per Capita',
+    'gdp': 'GDP',
+    'population': 'Population',
+    'employment_and_wages': 'Employment and Wages',
+    'total_wages': 'Total Wages {{lastYear}}',
+    'employment': 'Employment {{lastYear}}',
+    'exports_and_imports': 'Exports and imports',
+    'exports': 'Exports, {{year}}',
+    'export_possiblities': 'Export possiblities',
+    'export_possiblities.intro': "We've found that countries which export complex products grow faster than those which export simple products. Using the similarity map we built in the section above, we've highlighted  high potential products for {{name}}, ranked by which have the highest combination of opportunity and complexity.",
+    'export_possiblities.footer': 'Note that the list is dynamically generated.  Not all exports may make sense given local conditions not captured in our measure of product similarity.',
     'stepper': {
       1: `This circle represents bananas.
         Think of it as a container for all
@@ -160,5 +168,14 @@ export default {
         that has banana trees. We shade products green by their
         complexity—that is, how many countries export them.`
      }
+  },
+  'industry.show': {
+    'employment_and_wages': 'Employment and Wages',
+    'industries': 'Industries',
+    'value': 'Value',
+    'employment_growth': 'Employment Growth (2008-2013)',
+    'avg_wages': 'Average Wages {{year}}',
+    'employment': 'Employment {{year}}',
+    'industry_composition': 'Industry Composition, 2012'
   }
 };
