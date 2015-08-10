d848f47e922f1dfa4ca93b8b7574932b23203b42
Quinn Lee <leekeunhwi@gmail.com>
2015-08-10 09:39:33 -0400

    replace industry colors with industry section colors

---
+++ app/fixtures/industry_section_colors.js
@@ -0,0 +1,70 @@
+export default {
+  0: {
+    "name_es": "Agricultura, Ganadería, Caza Y Silvicultura",
+    "color": "#7ddaa1"
+  },
+  31: {
+    "name_es": "Pesca", //fishing
+    "color": '#c57bd9'
+  },
+  40: {
+    "name_es": "Explotación De Minas Y Canteras", //mining
+    "color": '#db987c'
+  },
+  79: {
+    "name_es": "Industrias Manufactureras",
+    "color": '#7ba2d9',
+  },
+  332: {
+    "name_es": "Suministro De Electricidad, Gas Y Agua", //electricity, gas and water
+    "color": '#b3b3b3'
+  },
+  346: {
+    "name_es": "Construcción",
+    "color": '#d9967b'
+  },
+  371: {
+    "name_es": "Comercio Al Por Mayor Y Al Por Menor; Reparación De Vehículos Automotores, Motocicletas, Efectos Personales Y Enseres Domésticos", //cars and everything about cars
+    'color': '#8d7bd8'
+  },
+  467: {
+    "name_es": "Hoteles Y Restaurantes",
+    'color': '#7ba2d9'
+  },
+  486: {
+    "name_es": "Transporte, Almacenamiento Y Comunicaciones",
+    "color": '#ca73c6'
+  },
+  544: {
+    "name_es": "Intermediación Financiera",
+    "color": '#d97ba8'
+  },
+  582: {
+    "name_es": "Actividades Inmobiliarias, Empresariales Y De Alquiler", //rent
+    'color': '#d9967b'
+  },
+  636: {
+    "name_es": "Administración Pública Y Defensa; Seguridad Social De Afiliación Obligatoria", //social services?
+    "color": '#7bd9a5'
+  },
+  655: {
+    "name_es": "Educación",
+    "color": '#7ddaa1'
+  },
+  680: {
+    "name_es": "Servicios Sociales Y De Salud",
+    "color": '#7bd9a5'
+  },
+  698: {
+    "name_es": "Otras Actividades De Servicios Comunitarios, Sociales Y Personales",
+    'color': '#abd97b'
+  },
+  736: {
+    "name_es": "Hogares Privados Con Servicio Doméstico",
+    'color': '#7ddaa1'
+  },
+  740: {
+    "name_es": "Organizaciones Y Órganos Extraterritoriales",
+    'color': '#7ddada'
+  }
+};
