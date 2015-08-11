b5b0dd1a131d18b9af1c65d18731e2f694cb7a04
Quinn Lee <leekeunhwi@gmail.com>
2015-08-10 22:59:17 -0400

    refactor application model

--- app/routes/application.js
+++ app/routes/application.js
@@ -9,11 +9,9 @@ export default Ember.Route.extend({
   model: function() {
 
     var products4digit = Ember.$.getJSON(apiURL+'/metadata/products?level=4digit');
-    var productsSection= Ember.$.getJSON(apiURL+'/metadata/products?level=section');
     var locationsMetadata = Ember.$.getJSON(apiURL+'/metadata/locations/');
     var productsHierarchy = Ember.$.getJSON(apiURL+'/metadata/products/hierarchy?from_level=4digit&to_level=section');
     var industriesClass = Ember.$.getJSON(apiURL+'/metadata/industries?level=class');
-    var industriesSection = Ember.$.getJSON(apiURL+'/metadata/industries?level=section');
     var industriesHierarchy = Ember.$.getJSON(apiURL+'/metadata/industries/hierarchy?from_level=4digit&to_level=section');
 
     var promises = [
@@ -22,8 +20,6 @@ export default Ember.Route.extend({
        productsHierarchy,
        industriesClass,
        industriesHierarchy,
-       productsSection,
-       industriesSection
     ];
 
     return RSVP.allSettled(promises).then(function(array) {
@@ -32,8 +28,6 @@ export default Ember.Route.extend({
       let productsHierarchy = array[2].value.data;
       let industriesMetadata = array[3].value.data;
       let industriesHierarchy = array[4].value.data;
-      let productSection = array[5].value.data;
-      let industrySection = array[6].value.data;
 
       // Finds the entity with the `1st digit` that matches
       // sets `group` to the `1st digit code`
@@ -42,21 +36,15 @@ export default Ember.Route.extend({
       _.forEach(productsMetadata, function(d) {
         let sectionId= productsHierarchy[d.id];
         let color = ProductSectionColor[sectionId].color;
-        let product = _.find(productSection, {id:  sectionId});
         d.color = color;
         d.group = sectionId;
-        d.group_name_en = product.name_en;
-        d.group_name_es = product.name_es;
       });
 
       _.forEach(industriesMetadata, function(d) {
         let sectionId = industriesHierarchy[d.id];
         let color = IndustrySectionColor[sectionId].color;
-        let industry = _.find(industrySection, { id: sectionId });
         d.group = sectionId;
         d.color = color;
-        d.group_name_en = industry.name_en;
-        d.group_name_en = industry.name_es;
       });
 
       // Index metadata by entity id's
