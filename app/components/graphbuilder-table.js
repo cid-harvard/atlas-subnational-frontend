f860cc234a1c4f751eb851142ddc84e063dd81b6
Quinn Lee <leekeunhwi@gmail.com>
2015-08-24 14:41:02 -0400

    resolve JS errors when re-sizing columns

--- app/components/graphbuilder-table.js
+++ app/components/graphbuilder-table.js
@@ -8,7 +8,31 @@ import TableCell from 'ember-table/views/table-cell';
 const { computed, observer } = Ember;
 
 var SortableTableHeaderCell = HeaderCell.extend({
-  templateName: 'sortable-header-cell'
+  templateName: 'sortable-header-cell' ,
+
+   // `event` here is a jQuery event
+  onColumnResize: function(event, ui) {
+    var newWidth = Math.round(ui.size.width);
+    if (this.get('tableComponent.columnMode') === 'standard') {
+      this.get('column').resize(newWidth);
+      this.set('tableComponent.columnsFillTable', false);
+    } else {
+      var diff = this.get('width') - newWidth;
+      this.get('column').resize(newWidth);
+      this.get('nextResizableColumn').resize(
+          this.get('nextResizableColumn.width') + diff);
+    }
+
+    this.elementSizeDidChange();
+
+    // Trigger the table resize (and redraw of layout) when resizing is done
+    if (event.type === 'resizestop') {
+      this.get('tableComponent').elementSizeDidChange();
+    }
+
+    this.get('tableComponent').sendAction('onColumnResized', this.get('column'), newWidth);
+  }
+
 });
 
 var SortableTableCell = TableCell.extend({
