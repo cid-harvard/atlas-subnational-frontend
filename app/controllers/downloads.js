1993ae566080c412dfffe9e88a0b1bd761deba9e
gwezerek <gwezerek@gmail.com>
2015-09-16 11:11:16 -0400

    Abstracted out downloads string, added remaining downloads.

---
+++ app/controllers/downloads.js
@@ -0,0 +1,6 @@
+import Ember from 'ember';
+
+export default Ember.Controller.extend({
+  downloadURL: 'https://s3.amazonaws.com/datlas-colombia-downloads/2dbddad8e5d5575559a83a5474534f89c70bc329'
+});
+
