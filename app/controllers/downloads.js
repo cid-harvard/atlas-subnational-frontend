403b54e5fdf550121988c943818ad59287cd9d6a
Quinn Lee <leekeunhwi@gmail.com>
2015-10-19 14:39:34 -0400

    add file download url to env file

--- app/controllers/downloads.js
+++ app/controllers/downloads.js
@@ -1,6 +1,7 @@
 import Ember from 'ember';
+import ENV from '../config/environment';
 
 export default Ember.Controller.extend({
-  downloadURL: 'https://s3.amazonaws.com/datlas-colombia-downloads/2dbddad8e5d5575559a83a5474534f89c70bc329'
+  downloadURL: ENV.downloadURL
 });
 
