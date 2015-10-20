0171e8dbac991e1c25e18ebb6986f22f2d297e80
Mali Akmanalp <mali@akmanalp.com>
2015-10-20 14:22:43 -0400

    Update mapping for industry section colors to match newer version of classification.

--- app/fixtures/industry_section_colors.js
+++ app/fixtures/industry_section_colors.js
@@ -1,75 +1,75 @@
 export default {
-  504: {
+  72: {
     "color": "#7ddaa1",
     "name_en": "Textiles & Furniture",
     "name_es": "Textiles y Muebles",
   },
-  505: {
+  0: {
     "color": "#dbd37d",
     "name_en": "Vegetables, Animals, Foodstuffs, Wood",
     "name_es": "Vegetales, Animales, Alimentos, y Madera",
   },
-  506: {
+  112: {
     "color": "#dab47d",
     "name_en": "Stone & Glass",
     "name_es": "Piedra y Vidrio",
   },
-  507: {
+  26: {
     "color": "#db987c",
     "name_en": "Minerals",
     "name_es": "Minerales",
   },
-  508: {
+  119: {
     "color": "#d97b7b",
     "name_en": "Metals",
     "name_es": "Metales",
   },
-  509: {
+  98: {
     "color": "#c57bd9",
     "name_en": "Chemicals & Plastics",
     "name_es": "Químicos y Plásticos",
   },
-  510: {
+  158: {
     "color": "#8d7bd8",
     "name_en": "Transportation",
     "name_es": "Vehículos de Transporte",
   },
-  511: {
+  136: {
     "color": "#7ba2d9",
     "name_en": "Machinery",
     "name_es": "Maquinaria",
   },
-  512: {
+  145: {
     "color": "#7ddada",
     "name_en": "Electronics",
     "name_es": "Electrónicos",
   },
-  513: {
+  356: {
     "color": "#7bd9a5",
     "name_en": "Social Services",
     "name_es": "Servicios Sociales",
   },
-  514: {
+  36: {
     "color": "#b3b3b3",
     "name_en": "Utilities",
     "name_es": "Servicios Públicos",
   },
-  515: {
+  43: {
     "color": "#d9967b",
     "name_en": "Construction",
     "name_es": "Construcción",
   },
-  516: {
+  234: {
     "color": "#ca73c6",
     "name_en": "Transport & Communications",
     "name_es": "Transporte y Comunicaciones",
   },
-  517: {
+  174: {
     "color": "#abd97b",
     "name_en": "Commerce",
     "name_es": "Comercio",
   },
-  518: {
+  301: {
     "color": "#d97ba8",
     "name_en": "Financial & Business Services",
     "name_es": "Servicios Financieros y Empresariales",
