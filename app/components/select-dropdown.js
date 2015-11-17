ea0dffb7e2ad6b654b44110995a68e7566a4d8c1
katybeck <katybeck@gmail.com>
2015-11-17 14:29:19 -0500

    integrate jqueryui selectmenu and style it

--- app/components/select-dropdown.js
+++ app/components/select-dropdown.js
@@ -3,9 +3,11 @@ const {computed} = Ember;
 
 export default Ember.Component.extend({
   didInsertElement: function() {
-    $("#select-location").selectmenu();
-    $("#select-industry").selectmenu();
-    $("#select-product").selectmenu();
+    $("#select-location").selectmenu()
+      .addClass( "overflow" );
+    $("#select-industry").selectmenu()
+      .addClass( "overflow" );
+    $("#select-product").selectmenu()
+      .addClass( "overflow" );
   }
 });
-
