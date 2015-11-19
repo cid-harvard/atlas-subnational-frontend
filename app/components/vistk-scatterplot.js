4ec310e50be9a03b9e8ba826928585eb1a9e5f5d
romsson <romain.vuillemot@gmail.com>
2015-11-19 10:55:05 -0500

    Updates on number format function

--- app/components/vistk-scatterplot.js
+++ app/components/vistk-scatterplot.js
@@ -16,6 +16,7 @@ export default Ember.Component.extend({
   scatter: computed('data.[]', 'dataType','eciValue','i18n.locale', function() {
     let eci = this.get('eciValue');
     let lang = this.get('i18n.locale') === 'en-col' ? 'en_EN': 'es_ES';
+    let format = function(value) { return numeral(value).format('0.0'); };
     return vistk.viz()
     .params({
       type: 'scatterplot',
@@ -67,15 +68,12 @@ export default Ember.Component.extend({
               'key': 'cog',
               'value': d['cog']
             }];
-            function format(key, value) {
-              return numeral(value).format('0.0');
-            }
             var textItem = get(d, `name_short_${this.get('i18n').display}`) || d.code;
             var tooltip_text = `<span style="color:${get(d, 'color')}">${textItem} - ${get(d, 'code')}</span>`;
 
             data.forEach((d) => {
               if(d.key) {
-                tooltip_text += '<br>' + this.get('i18n').t(`graph_builder.table.${get(d,'key')}`) + ': ' + format(get(d,'key'), get(d,'key'));
+                tooltip_text += '<br>' + this.get('i18n').t(`graph_builder.table.${get(d,'key')}`) + ': ' + format(get(d,'key'));
               }
             });
 
@@ -88,7 +86,7 @@ export default Ember.Component.extend({
           type: 'line_horizontal',
           filter: function(d, i) {
            return typeof eci !== 'undefined' && i === 0;
-         },
+          },
           offset_y: function(d, i, vars) {
             return -(vars.y_scale[0]['func'](d[vars.var_y]) - vars.y_scale[0]['func'](eci));
           }
