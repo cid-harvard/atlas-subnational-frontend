31f2dc015409be965c49a5cbdb097cc53e4fa9ff
Quinn Lee <leekeunhwi@gmail.com>
2015-12-08 10:37:34 -0500

    COL-731 components for dropdown

--- app/components/select-dropdown.js
+++ app/components/select-dropdown.js
@@ -1,13 +1,32 @@
 import Ember from 'ember';
-const {computed} = Ember;
+const {computed, observer, set, get} = Ember;
 
 export default Ember.Component.extend({
-  didInsertElement: function() {
-    $("#select-location").selectmenu()
-      .addClass( "overflow" );
-    $("#select-industry").selectmenu()
-      .addClass( "overflow" );
-    $("#select-product").selectmenu()
-      .addClass( "overflow" );
+  i18n: Ember.inject.service(),
+  transitionProduct: 'transitionProduct',
+  transitionLocation: 'transitionLocation',
+  transitionIndustry: 'transitionIndustry',
+  locale: computed.alias('i18n.locale'),
+  didInsertElement() {
+    let locale = get(this, 'i18n.locale');
+    this.$('select').selectmenu({
+     select: (event, ui) =>{
+       let id = get(ui, 'item.value');
+       let type = get(this, 'type');
+       if(type === 'location') {
+         this.sendAction('transitionLocation', id);
+       } else if (type === 'product') {
+         this.sendAction('transitionProduct', id);
+       } else if (type === 'industry') {
+         this.sendAction('transitionIndustry', id);
+       }
+     }
+    }).addClass(`${locale} overflow`);
+
+    this.addObserver('locale', () => {
+      Ember.run.next(this, function() {
+        this.$('select').selectmenu('refresh', true);
+      });
+    })
   }
 });
