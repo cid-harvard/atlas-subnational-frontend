25220d5a979285733c8d3a595af4f5b7ecfd0f27
Quinn Lee <leekeunhwi@gmail.com>
2015-12-08 10:37:23 -0500

    COL-731 get data for drop downs

--- app/controllers/index.js
+++ app/controllers/index.js
@@ -1,13 +1,23 @@
 import Ember from 'ember';
-
-const {computed, get:get} = Ember;
+const {computed, observer, get} = Ember;
 
 export default Ember.Controller.extend({
   i18n: Ember.inject.service(),
 
   firstYear: computed.alias('i18n.firstYear'),
   lastYear: computed.alias('i18n.lastYear'),
-
+  products: computed.filter('model.products', (product, index, array) => {
+    let id = [1143, 87];
+    return _.contains(id, parseInt(get(product, 'id')));
+  }),
+  cities: computed.filter('model.cities', (city, index, array) => {
+    let id = [41,87, 34, 40];
+    return _.contains(id, parseInt(get(city, 'id')));
+  }),
+  industries: computed.filter('model.industries', (industry, index, array) => {
+    let id = [461, 488];
+    return _.contains(id, parseInt(get(industry, 'id')));
+  }),
   locationFirst: computed('i18n', function() {
     return get(this, 'i18n').t('index.location_q1.id').string;
   }),
@@ -32,4 +42,15 @@ export default Ember.Controller.extend({
   graphbuilder: computed('i18n', function() {
     return get(this, 'i18n').t('index.graphbuilder.id').string;
   }),
+  actions: {
+    transitionLocation(id) {
+      this.transitionToRoute('location.show', id);
+    },
+    transitionProduct(id) {
+      this.transitionToRoute('product.show', id);
+    },
+    transitionIndustry(id) {
+      this.transitionToRoute('industry.show', id);
+    }
+  }
 });
