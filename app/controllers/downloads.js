b44839b85c6deb5b72d53df04e06f7aac828be62
Quinn Lee <leekeunhwi@gmail.com>
2015-12-09 15:49:02 -0500

    Feature toggle for col

--- app/controllers/downloads.js
+++ app/controllers/downloads.js
@@ -2,6 +2,7 @@ import Ember from 'ember';
 import ENV from '../config/environment';
 
 export default Ember.Controller.extend({
+  featureToggle: Ember.inject.service(),
   downloadURL: ENV.downloadURL
 });
 
