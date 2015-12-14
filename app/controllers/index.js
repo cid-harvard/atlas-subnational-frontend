eb31e4708c57873948402ccf0e4a17b3760a295b
Quinn Lee <leekeunhwi@gmail.com>
2015-12-14 10:46:07 -0500

    Refactor out drop down objects to be from locale file.  Also preserve order from locale.

--- app/controllers/index.js
+++ app/controllers/index.js
@@ -6,17 +6,59 @@ export default Ember.Controller.extend({
 
   firstYear: computed.alias('i18n.firstYear'),
   lastYear: computed.alias('i18n.lastYear'),
-  products: computed.filter('model.products', (product) => {
-    let id = [1143, 87];
-    return _.contains(id, parseInt(get(product, 'id')));
+  productIds: computed(function() {
+    return get(this, 'i18n')
+      .t('index.dropdown.products')
+      .string
+      .split(',');
   }),
-  cities: computed.filter('model.cities', (city) => {
-    let id = [41,87, 34, 40];
-    return _.contains(id, parseInt(get(city, 'id')));
+  locationIds: computed(function() {
+    return get(this, 'i18n')
+      .t('index.dropdown.locations')
+      .string
+      .split(',');
   }),
-  industries: computed.filter('model.industries', (industry) => {
-    let id = [461, 488];
-    return _.contains(id, parseInt(get(industry, 'id')));
+  industryIds: computed(function() {
+    return get(this, 'i18n')
+      .t('index.dropdown.industries')
+      .string
+      .split(',');
+  }),
+  products: computed.filter('model.products', function (product) {
+    let id = get(this, 'productIds');
+    return _.contains(id, get(product, 'id'));
+  }),
+  sortedProducts: computed('productIds', 'products', function() {
+    let id = get(this, 'productIds');
+    let products = get(this, 'products');
+
+    return _.map(id, function(d) {
+      return _.find(products, { id: d });
+    });
+  }),
+  cities: computed.filter('model.cities', function (city) {
+    let id = get(this, 'locationIds');
+    return _.contains(id, get(city, 'id'));
+  }),
+  sortedCities: computed('locationIds', 'cities', function() {
+    let id = get(this, 'locationIds');
+    let cities = get(this, 'cities');
+
+    return _.map(id, function(d) {
+      return _.find(cities, { id: d });
+    });
+  }),
+  industries: computed.filter('model.industries', function (industry) {
+    let id = get(this, 'industryIds');
+    return _.contains(id, get(industry, 'id'));
+  }),
+  sortedIndustries: computed('industryIds', 'industries', function() {
+    let id = get(this, 'industryIds');
+    let industries = get(this, 'industries');
+
+    return _.map(id, function(d) {
+      return _.find(industries, { id: d });
+    });
   }),
   locationFirst: computed('i18n', function() {
     return get(this, 'i18n').t('index.location_q1.id').string;
