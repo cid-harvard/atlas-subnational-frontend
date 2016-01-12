82640ec8aef037b77fc3444702ba940f21b4913b
katybeck <katybeck@gmail.com>
2016-01-12 15:50:42 -0500

    needsLegend is better name; remove debug output in title

--- app/controllers/visualization.js
+++ app/controllers/visualization.js
@@ -70,7 +70,7 @@ export default Ember.Controller.extend({
     let legend = this.get(`metadata.legend.${this.get('source')}`);
     return _.values(legend);
   }),
-  showLegend: computed('model.visualization', function() {
+  needsLegend: computed('model.visualization', function() {
     let vis = this.get('model.visualization');
      return _.contains(['scatter', 'similarity'], vis) ? true : false;
   }),
