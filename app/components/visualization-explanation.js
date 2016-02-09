d60213a53aaabc36bb80671914e5e3bc245bf823
Quinn Lee <leekeunhwi@gmail.com>
2016-02-09 11:03:38 -0500

    hot fix.  Remove explanation button,  show all decrease text size.  CSS was broken for IE/Firefox

--- app/components/visualization-explanation.js
+++ app/components/visualization-explanation.js
@@ -1,21 +1,3 @@
 import Ember from 'ember';
-const {computed, set, get:get, on} = Ember;
-
-export default Ember.Component.extend({
-  isTextLarge: computed('text', function() {
-    return get(this, 'text.string.length')> 300;
-  }),
-  buttonText: computed('showAll', function() {
-    if(get(this, 'showAll')) { return 'graph_builder.explanation.hide'; }
-    return 'graph_builder.explanation.show';
-  }),
-  setShowAll: on('init', function() {
-    set(this, 'showAll', !get(this, 'isTextLarge'));
-  }),
-  actions: {
-    toggleText: function() {
-      this.toggleProperty('showAll');
-    }
-  }
-});
+export default Ember.Component.extend({});
 
