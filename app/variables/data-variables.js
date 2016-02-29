8f02b143e08bff70aae026c9bb9f5a8111db7aea
Quinn Lee <leekeunhwi@gmail.com>
2016-02-29 14:46:55 -0500

    Mex variables

--- app/variables/data-variables.js
+++ app/variables/data-variables.js
@@ -16,4 +16,23 @@ export default  {
     "census_year": 2014,
     "geo_center": [4.6,-74.06]
   },
+  mex: {
+    "index_products": [1143,87],
+    "index_locations": [2533, 2501, 2511, 2539],
+    "index_industries": [294, 359],
+    "index_location_q1_id": 378,
+    "index_location_q2_id": 291,
+    "index_industry_q1_id": 294,
+    "index_industry_q2_id": 359,
+    "index_product_q1_id": 74,
+    "index_product_q2_id": 1143,
+    "index_profile_id": 206,
+    "index_graphbuilder_id": 87,
+    "last_year": 2014,
+    "first_year": 2004,
+    "census_year": 2010,
+    "geo_center": [19,-99]
+  },
+  peru: {
+  }
 };
