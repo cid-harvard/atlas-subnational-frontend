79e2540697b0c37051f6e5eceb7c695f34fe3350
Quinn Lee <leekeunhwi@gmail.com>
2016-03-10 11:38:13 -0500

    PERU-85 feature toggle cities on rankings page

--- app/controllers/rankings.js
+++ app/controllers/rankings.js
@@ -3,6 +3,7 @@ const { computed, get } = Ember;
 
 export default Ember.Controller.extend({
   i18n: Ember.inject.service(),
+  featureToggle: Ember.inject.service(),
   pageTitle: computed('i18n.locale', function() {
     return get(this, 'i18n').t('rankings.pagetitle');
   }),
