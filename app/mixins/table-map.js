881ce94139c7149c8e10723275478e53e8b049cc
Mali Akmanalp <mali@akmanalp.com>
2016-03-29 15:41:33 -0400

    Table header in partner countries showed imports instead of exports PERU-132

--- app/mixins/table-map.js
+++ app/mixins/table-map.js
@@ -139,7 +139,7 @@ export default Ember.Mixin.create({
       { key: 'code' },
       { key: 'name', copy: 'country' },
       { key: 'parent', copy: 'parent.country' },
-      { key: 'import_value' },
+      { key: 'export_value' },
       { key: 'year' }
     ];
 
