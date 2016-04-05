730c158cd5c675480867ff586881cc4c3da51c3f
romsson <romain.vuillemot@gmail.com>
2016-04-05 15:19:57 +0200

    WIP to test charts dynamic filtering

--- app/components/visualization-legend.js
+++ app/components/visualization-legend.js
@@ -4,11 +4,31 @@ export default Ember.Component.extend({
   activeKey: null,
   actions: {
     changeKey(key) {
+      let code = [];
+
       if(this.get('activeKey') === key) {
         this.set('activeKey', null);
       } else {
         this.set('activeKey', key);
+        code = [key];
       }
+
+      // Retrieve the scatterplot's configuration object
+      let elScatter = this.get('parentView').get('childViews').filter(function(d) {
+        return typeof d['scatter'] !== 'undefined';
+      })[0];
+
+      // Update the scatterplot's configuration object
+      elScatter.get('scatter').params({
+        filter: code
+      });
+
+      // Force graph refresh with new configuration
+      elScatter.get('scatter').params().refresh = true;
+
+      // Re-draw
+      d3.select(elScatter.get('id')).call(elScatter.get('scatter'));
+
     }
   }
 });
