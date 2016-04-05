87a0c2988a0c3a33c681ece43a5c707a3166ee74
romsson <romain.vuillemot@gmail.com>
2016-04-05 15:08:27 +0200

    Toggling active key coming from downstream

--- app/components/visualization-legend.js
+++ app/components/visualization-legend.js
@@ -1,4 +1,14 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
+  activeKey: null,
+  actions: {
+    changeKey(key) {
+      if(this.get('activeKey') === key) {
+        this.set('activeKey', null);
+      } else {
+        this.set('activeKey', key);
+      }
+    }
+  }
 });
