5e67542f3db93ee34ee57f823d45569038ab110d
romsson <romain.vuillemot@gmail.com>
2016-04-05 15:06:20 +0200

    Adding active/inactive toggle and class binding

--- app/components/visualization-legend-key.js
+++ app/components/visualization-legend-key.js
@@ -1,8 +1,9 @@
 import Ember from 'ember';
-const { computed, get } = Ember;
+const {computed, get} = Ember;
 
 export default Ember.Component.extend({
   color: computed.alias('key.color'),
+  code: computed.alias('key.code'),
   name: computed('key', 'i18n.locale', function(){
     let locale = get(this, 'i18n.display');
     return get(this, `key.name_${locale}`);
@@ -10,5 +11,10 @@ export default Ember.Component.extend({
   myStyle: computed('color', function() {
     var color = this.get('color');
     return new Ember.Handlebars.SafeString("color: " + color);
+  }),
+  classNameBindings: ['isActive'],
+  active: false,
+  isActive: computed('active', function() {
+    return this.get('code') === this.get('active');
   })
 });
