53475bec177ff6c06544cbd41c881baa773dba04
romsson <romain.vuillemot@gmail.com>
2016-04-15 15:44:18 +0200

    Making sure filter don't color non-exported products

--- app/components/vistk-network.js
+++ app/components/vistk-network.js
@@ -75,7 +75,7 @@ export default Ember.Component.extend({
             //if there is no search, color products export > 0 and rca > 1
             // industries if RCA > 1 ( varDependent for industries is also rca )
             if(keyFilter.length > 0) {
-              if(d[vars.var_group] === keyFilter[0]) {
+              if(d[vars.var_group] === keyFilter[0] && d[this.get('varRCA')] >= 1) {
                 return d.color;
               } else {
                 return 'white';
@@ -169,7 +169,7 @@ export default Ember.Component.extend({
               type: 'circle',
               fill: (d, i, vars) => {
                 if(keyFilter.length > 0) {
-                  if(d[vars.var_group] === keyFilter[0]) {
+                  if(d[vars.var_group] === keyFilter[0] && d[this.get('varRCA')] >= 1) {
                     return d.color;
                   } else {
                     return 'white';
