80370c65ab14fe8fc6f8d04434a73c6332368d48
Mali Akmanalp <mali@akmanalp.com>
2016-04-27 16:33:25 -0400

    Add missing computed() dependencies

--- app/controllers/downloads.js
+++ app/controllers/downloads.js
@@ -6,7 +6,7 @@ const { computed } = Ember;
 export default Ember.Controller.extend({
   featureToggle: Ember.inject.service(),
   downloadURL: `${ENV.downloadURL}/production`,
-  downloadFiles: computed(function(){
+  downloadFiles: computed('downloadURL', 'featureToggle.download_files', function(){
     let prefix = this.get('downloadURL');
     return _.mapValues(
       this.get('featureToggle.download_files'),
