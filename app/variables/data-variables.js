31c6d10667e39fbeea4552583ae639cf0188c8f9
Mali Akmanalp <mali@akmanalp.com>
2016-05-07 19:19:20 -0400

    Add current peru hero image PERU-104

--- app/variables/data-variables.js
+++ app/variables/data-variables.js
@@ -133,7 +133,7 @@ export default  {
       "country": "department",
       "department": "msa",
     },
-    "hero_image": "/assets/img/hero_images/front-header-treemap.png",
+    "hero_image": "/assets/img/hero_images/front-header-peru.png",
     "show_downloads": true,
     "homepage_logos": [
       {
