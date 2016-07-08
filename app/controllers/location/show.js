cb3576400f41687cb2b927de8fbfd38687ec3f80
Mali Akmanalp <mali@akmanalp.com>
2016-07-08 12:13:26 -0400

    Mexico style changes, top navbar, etc.

--- app/controllers/location/show.js
+++ app/controllers/location/show.js
@@ -10,6 +10,7 @@ export default Ember.Controller.extend({
   firstYear: computed.alias('featureToggle.first_year'),
   lastYear: computed.alias('featureToggle.last_year'),
 
+  hasOccupationsData: computed.notEmpty('occupationsData'),
   validTimeseries: computed.alias('model.timeseries'),
   dotplotData: computed.oneWay('model.dotplotData'),
   occupationData: computed.oneWay('model.occupations'),
