d8db4b3761056c4a45622f34f544f65f06423972
Mali Akmanalp <mali@akmanalp.com>
2016-07-08 12:19:21 -0400

    Mexico tests.

--- tests/acceptance/search-by-code-test.js
+++ tests/acceptance/search-by-code-test.js
@@ -19,20 +19,14 @@ module('Acceptance | search by code', {
 test('search by code at Search', function(assert) {
 
   //Industry
-  visit('/search?query=0111');
+  visit('/search?query=61&filter=industry');
   andThen(function() {
-    assert.equal(find('p[data-search-result-name]:first').text(), 'Coffee');
+    assert.equal(find('p[data-search-result-name]:first').text(), 'Educational services');
   });
 
   //product
-  visit('/search?query=0105');
+  visit('/search?query=0105&filter=product');
   andThen(function() {
     assert.equal(find('p[data-search-result-name]:first').text(), 'Fowl');
-  });
-
-  //location
-  visit('/search?query=05');
-  andThen(function() {
-    assert.equal(find('p[data-search-result-name]:first').text(), 'Antioquia');
   });
 });
