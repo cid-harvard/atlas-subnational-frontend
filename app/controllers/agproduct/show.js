14501adf0184d609d7ca384814b5af4793252da7
Mali Akmanalp <mali@akmanalp.com>
2017-07-20 21:45:04 -0400

    Add agproduct profiles.

---
+++ app/controllers/agproduct/show.js
@@ -0,0 +1,12 @@
+import Ember from 'ember';
+const {computed} = Ember;
+
+export default Ember.Controller.extend({
+  featureToggle: Ember.inject.service(),
+  queryParams: ['year'],
+
+  firstYear: computed.alias('featureToggle.first_year'),
+  lastYear: computed.alias('featureToggle.last_year'),
+});
+
+
